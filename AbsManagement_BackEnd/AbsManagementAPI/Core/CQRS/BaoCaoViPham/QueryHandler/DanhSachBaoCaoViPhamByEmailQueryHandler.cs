8c7d03533405e14a61c2ac6601e019523a00eefa
LINH\asus <ductinh280499@gmail.com>
2024-01-07 13:10:53 +0700

    Viết api lấy danh sách báo cáo vi phạm theo mail

--- AbsManagement_BackEnd/AbsManagementAPI/Core/CQRS/BaoCaoViPham/QueryHandler/DanhSachBaoCaoViPhamByEmailQueryHandler.cs
+++ AbsManagement_BackEnd/AbsManagementAPI/Core/CQRS/BaoCaoViPham/QueryHandler/DanhSachBaoCaoViPhamByEmailQueryHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace AbsManagementAPI.Core.CQRS.BaoCaoViPham.QueryHandler
+﻿using AbsManagementAPI.Core.CQRS.BaoCaoViPham.Query;
+using AbsManagementAPI.Core.Entities;
+using AbsManagementAPI.Core.Models.BaoCaoViPham;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbsManagementAPI.Core.CQRS.BaoCaoViPham.QueryHandler
 {
-    public class DanhSachBaoCaoViPhamByEmailQueryHandler
+    public class DanhSachBaoCaoViPhamByEmailQueryHandler : BaseHandler, IRequestHandler<DanhSachBaoCaoViPhamByEmailQuery, List<BaoCaoViPhamModel>>
     {
+        public DanhSachBaoCaoViPhamByEmailQueryHandler(IHttpContextAccessor context, DataContext dataContext, IMapper mapper) : base(context, dataContext, mapper)
+        {
+        }
+
+        public async Task<List<BaoCaoViPhamModel>> Handle(DanhSachBaoCaoViPhamByEmailQuery request, CancellationToken cancellationToken)
+        => await _dataContext.BaoCaoViPhams.Include(t => t.CanBoXuLy).Where(t => t.Email == request.Email)
+            .ProjectTo<BaoCaoViPhamModel>(_mapper.ConfigurationProvider).ToListAsync(cancellationToken);
     }
 }
